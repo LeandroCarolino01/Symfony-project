e55f108f3306cae76c9d5beab14d2a87cccfe116
Leandro.Carolino <carolino.leandro@gmail.com>
2018-10-06 14:17:06 +0200

    first commit

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,6 +1,8 @@
 <?php
     namespace App\Controller;
 
+    use App\Entity\Article;
+
     use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\Routing\Annotation\Route;
     use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -8,14 +10,23 @@
 
     class ArticleController extends Controller {
         /**
-         * @Route("/")
+         * @Route("/", name="article_list")
          * @Method({"GET"})
          */
         public function index() {
             //return new Response('<html><body>Hello</body></html>');
 
-            $articles = ['Articles 1', 'Article 2'];
+            $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
 
             return $this->render('articles/index.html.twig', array('articles' => $articles));
         }
+
+         /**
+         * @Route("/article/{id}", name="article_show")
+         */
+        public function show($id) {
+            $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+            return $this->render('articles/show.html.twig', array('article' => $article));
+        }
+            
     }
